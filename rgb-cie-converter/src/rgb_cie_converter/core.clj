512bcc96837a5bc2b5bf814a9dde6a8fb69d1feb
Lamar Payson <glp@glpaysons-MBP.192.168.1.1>
2015-05-28 23:48:10 -0400

    thread-last-ing
    nt

--- rgb-cie-converter/src/rgb_cie_converter/core.clj
+++ rgb-cie-converter/src/rgb_cie_converter/core.clj
@@ -1,20 +1,17 @@
 (ns rgb-cie-converter.core
   (:require [clojure.math.numeric-tower :as math]))
 
-(defn normalize-color
-  [c]
+(defn normalize-color [c]
   (/ c 255.0))
 
-(defn enhance-color
-  [c]
+(defn enhance-color [c]
   (if (> c 0.04045)
     (math/expt
       (/ (+ c 0.055) 1.055)
       2.4)
     (/ c 12.92)))
 
-(defn to-xyz
-  [rgb]
+(defn to-xyz [rgb]
   (let [[r g b] rgb]
     [(+
        (* r 0.649926)
@@ -29,18 +26,17 @@
        (* g 0.053077)
        (* b 1.035763))]))
 
-(defn to-xy
-  [xyz]
+(defn to-xy [xyz]
   (let [[x y z] xyz]
     (if (= (+ x y z) 0.0)
       [1 1]
       [(/ x (+ x y z))
        (/ y (+ x y z))])))
 
-(defn rgb-to-cie-space
-  [r g b]
-  (to-xy
-    (to-xyz
-      (map enhance-color
-           (map normalize-color [r g b])))))
+(defn rgb-to-cie-space [r g b]
+  (->>
+    (map normalize-color [r g b])
+    (map enhance-color)
+    (to-xyz)
+    (to-xy)))
 
