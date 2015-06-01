6042783ed47a02c1e765d566f6965d8919f646c9
Lamar Payson <glp@glpaysons-MBP.192.168.1.1>
2015-05-31 21:27:48 -0400

    adding conversion test

--- rgb-cie-converter/test/rgb_cie_converter/core_test.clj
+++ rgb-cie-converter/test/rgb_cie_converter/core_test.clj
@@ -1,7 +1,15 @@
 (ns rgb-cie-converter.core-test
   (:require [clojure.test :refer :all]
-            [rgb-cie-converter.core :refer :all]))
+            [rgb-cie-converter.core :refer :all :as converter]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+;; Test values are from http://www.developers.meethue.com/documentation/hue-xy-values
+
+(deftest rgb-to-cie-test
+  (let [expected-x 0.29918957031149185
+        expected-y 0.320790072802549
+        r 239
+        g 247
+        b 255]
+    (testing "Convert Alice Blue RGB to CIE."
+      (is (= [expected-x expected-y]
+             (converter/rgb-to-cie r g b))))))
